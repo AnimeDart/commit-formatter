aa0c1f4776bfb3426f95254385d9437229385e01
Laks <lakhsmycastro336@gmail.com>
2021-02-21 18:21:33 -0300

    ⚠️ Test(Commitlint): Add commitlint tests for test type

--- test/formatters/commitlint/commitlint_formatter_test.dart
+++ test/formatters/commitlint/commitlint_formatter_test.dart
@@ -5,10 +5,23 @@ void main() {
   final commitlintFormatter = CommitlintFormatter();
 
   group('class CommitlintFormatter { ... }', () {
-    test('Basic feature', () {
+    test('Basic feature - Fix Type', () {
       expect(
         commitlintFormatter.formatThis('Fix,Backend,Closes Issue #40'),
         '${CommitlintFormatter.emojis["FIX"]} Fix(Backend): Closes issue #40',
+      );
+    });
+    test('Basic feature - Test Type', () {
+      expect(
+        commitlintFormatter.formatThis('test, Backend , Add tests for core'),
+        '''${CommitlintFormatter.emojis["TEST"]} Test(Backend): Add tests for core''',
+      );
+    });
+    test('Basic feature - Invalid Test Type', () {
+      expect(
+        () => commitlintFormatter
+            .formatThis('tests, Backend , Add tests for core'),
+        throwsA(isA<InvalidCommitTypeException>()),
       );
     });
     test('Throws Invalid Type when a invalid type is provided', () {
